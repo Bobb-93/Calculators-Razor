23d221e53a9733c9af63685aab20ca23477f628a
Boris Yakimov <borisiakimov@gmail.com>
2022-02-13 20:08:27 +0200

    First commit

---
+++ Older-Calculators/Pages/CreditCalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Older_Calculators.Pages
+{
+    public class CreditCalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Older-Calculators/Pages/LeasingCalculator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Older_Calculators.Pages
+{
+    public class LeasingCalculatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
